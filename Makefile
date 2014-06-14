d60220f8226013aec45f9253d59ccdfa3bee6049
Takashi Unuma @ nimbus <nimbus.dpri.kyoto-u.ac.jp>
2014-06-14 10:37:10 +0900

    Modified Makefile options and color var setting in gmtplot_xz.sh

--- Makefile
+++ Makefile
@@ -1,66 +1,85 @@
 #
 # Makefile for ncedit.f90
 # original makefile coded by Takashi Unuma, Kyoto Univ.
-# Last modified: 2014/06/13
+# Last modified: 2014/06/14
 #
+
+#COMPILER=INTEL
+#COMPILER=GNU
+COMPILER=PGI
 
 #-------------------------------------------------
 # Make for Intel Compiler on Linux x86_64 system
-FC      = ifort
-#NETCDF  = /home/unuma/usr/local/netcdf-4.1.3
-#ZLIB    = /home/unuma/usr/local/zlib-1.2.5
-#HDF5    = /home/unuma/usr/local/hdf5-1.8.7
-NETCDF  = /LARGE0/gr10053/b31894/lib/netcdf-4.1.3-intel
-ZLIB    = /LARGE0/gr10053/b31894/lib/hdf5-1.8.7-intel
-HDF5    = /LARGE0/gr10053/b31894/lib/zlib-1.2.5-intel
-#FFLAGS  = -I$(NETCDF)/include -FR -O3 -xHost -assume byterecl -i-dynamic -fno-alias -unroll0 -ipo
-FFLAGS  = -I$(NETCDF)/include -FR -g -O0 -assume byterecl -i-dynamic -warn all -check all
+ifeq ($(COMPILER),INTEL)
+FC	= ifort
+#FFLAGS	= -FR -O3 -xHost -assume byterecl -i-dynamic -fno-alias -unroll0 -ipo
+FFLAGS	= -FR -g -O0 -assume byterecl -i-dynamic -warn all -check all
+# -- rx2000,gpgpu
+#NETCDF	= /home/unuma/usr/local/netcdf-4.1.3
+#ZLIB	= /home/unuma/usr/local/zlib-1.2.5
+#HDF5	= /home/unuma/usr/local/hdf5-1.8.7
+# -- kudpc
+NETCDF	= /LARGE0/gr10053/b31894/lib/netcdf-4.1.3-intel
+ZLIB	= /LARGE0/gr10053/b31894/lib/hdf5-1.8.7-intel
+HDF5	= /LARGE0/gr10053/b31894/lib/zlib-1.2.5-intel
+endif
 # -----
 # Make for GNU Compiler on Linux x86_64 system
-#FC      = gfortran
+ifeq ($(COMPILER),GNU)
+FC      = gfortran
+#FFLAGS	 = -frecord-marker=4 -ffree-form -O3 -ftree-vectorize -funroll-loops -fno-range-check
+FFLAGS	 = -frecord-marker=4 -ffree-form -O -Wall -Wuninitialized -ffpe-trap=invalid,zero,overflow -fbounds-check -fno-range-check
+# -- rx2000,gpgpu
 #NETCDF  = /home/unuma/usr/local/netcdf-4.1.3-gnu
 #ZLIB    = /home/unuma/usr/local/zlib-1.2.5-gnu
 #HDF5    = /home/unuma/usr/local/hdf5-1.8.7-gnu
-#NETCDF  = /usr
-#ZLIB    = /usr
-#HDF5    = /usr
+# -- qsmcs,mcs
+NETCDF  = /usr
+ZLIB    = /usr
+HDF5    = /usr
+# -- kudpc
 #NETCDF  = /LARGE0/gr10053/b31894/lib/netcdf-4.1.3-gnu
 #ZLIB    = /LARGE0/gr10053/b31894/lib/hdf5-1.8.12-gnu
 #HDF5    = /LARGE0/gr10053/b31894/lib/zlib-1.2.8-gnu
-#FFLAGS	= -I$(NETCDF)/include -frecord-marker=4 -ffree-form -O3 -ftree-vectorize -funroll-loops -fno-range-check
-#FFLAGS	= -I$(NETCDF)/include -frecord-marker=4 -ffree-form -O -Wall -Wuninitialized -ffpe-trap=invalid,zero,overflow -fbounds-check -fno-range-check
+endif
 # -----
 # Make for PGI Compiler on Linux x86_64 system
-#FC      = pgfortran
-#NETCDF  = /usr/local/netcdf-4.1.3-pgi
-#ZLIB    = /usr/local/zlib-1.2.5-pgi
-#HDF5    = /usr/local/hdf5-1.8.7-pgi
+ifeq ($(COMPILER),PGI)
+FC	= pgfortran
+FFLAGS	= -pc 64 -Kieee -O0 -Ktrap=fp -Minform=inform -Mbounds -Mlre=noassoc
+# -- nimbus
+NETCDF	= /usr/local/netcdf-4.1.3-pgi
+ZLIB	= /usr/local/zlib-1.2.5-pgi
+HDF5	= /usr/local/hdf5-1.8.7-pgi
+# -- kudpc
 #NETCDF  = /LARGE0/gr10053/b31894/lib/netcdf-4.1.3-pgi
 #ZLIB    = /LARGE0/gr10053/b31894/lib/hdf5-1.8.12-pgi
 #HDF5    = /LARGE0/gr10053/b31894/lib/zlib-1.2.8-pgi
-#FFLAGS	= -I$(NETCDF)/include -pc 64 -Kieee -O0 -Ktrap=fp -Minform=inform -Mbounds -Mlre=noassoc
+endif
+#-------------------------------------------------
+#-- You shouldn't need to change anything below here
 #-------------------------------------------------
 
 
-#LDFLAGS = $(NETCDF)/lib/libnetcdff.la $(NETCDF)/lib/libnetcdf.la -L$(ZLIB)/lib -L$(HDF5)/lib
-LDFLAGS = -L$(NETCDF)/lib -L$(ZLIB)/lib -L$(HDF5)/lib
-LIBS    = -lhdf5_hl -lhdf5 -lm -lcurl -lnetcdf -lnetcdff -lz
+OUTPUTINC = -I$(NETCDF)/include -I$(HDF5)/include -I$(ZLIB)/include
+OUTPUTLIB = -L$(NETCDF)/lib -L$(HDF5)/lib -L$(ZLIB)/lib
+LINKOPTS  = -lnetcdf -lnetcdff -lhdf5_hl -lhdf5 -lz -lm -lcurl
 
 OBJECT  = ncedit.o
 
 .SUFFIXES:
 .SUFFIXES: .f90 .o
 
-all: ncedit
+all: 	ncedit
 
 ncedit: $(OBJECT)
-	libtool --tag=F77 --mode=link $(FC) $(FFLAGS) $(LDFLAGS) -o $@ $(OBJECT) $(LIBS)
+	$(FC) $(OBJECT) $(FFLAGS) $(OUTPUTINC) $(OUTPUTLIB) $(LINKOPTS) -o $@
 
 .f90.o:
-	$(FC) $(FFLAGS) -c -o $@ $*.f90
+	$(FC) $(FFLAGS) $(OUTPUTINC) -c -o $@ $*.f90
 
 clean:
-	rm -rf *.o *genmod.* *.f90~ Makefile~ ncedit .libs
+	rm -rf *.o *genmod.* *.f90~ Makefile~ ncedit
 
 ############################################################ end
 .NOEXPORT:
--- gmtplot_xz.sh
+++ gmtplot_xz.sh
@@ -2,7 +2,7 @@
 #
 # GMTPLOT
 # original script coded by Takashi Unuma, Kyoto Univ.
-# Last modified: 2014/06/12
+# Last modified: 2014/06/14
 #
 
 if test $# -lt 1 ; then
@@ -15,33 +15,72 @@ for infile in $* ; do
 
 echo ${infile}
 
-title="Qc+Qr+Qi+Qs+Qg"
-unit="[g/kg]"
-VARMIN=0.5
-VARMAX=4.0
-VARINT=0.5
-BINT=a1f0.5
+prefix=$(echo ${infile} | cut -d "_" -f 1)
+suffix=$(echo ${infile} | cut -d "_" -f 2)
+ftime=${suffix%.nc4}
 
-#title="VERTICAL VELOCITY"
-#unit="[m/s]"
-#VARMIN=-5.0
-#VARMAX=5.0
-#VARINT=1.0
-#VARTYPE="polar"
-#BINT=a2f1
+auto=0
+if test ${prefix} = "water" ; then
+    title="Qc+Qr+Qi+Qs+Qg"
+    unit="[g/kg]"
+    VARMIN=0.5
+    VARMAX=4.0
+    VARINT=0.5
+    VARTYPE="seis"
+    BINT=a1f0.5
+elif test ${prefix} = "dbz" ; then
+    title="PSEUDO-REFLECTIVITY"
+    unit="[dBZ]"
+    VARMIN=10.0
+    VARMAX=60.0
+    VARINT=5.0
+    VARTYPE="seis"
+    BINT=a10f5
+elif test ${prefix} = "thetae" ; then
+    title="EQUIVALENT POTENTIAL TEMPERATURE"
+    unit="[K]"
+    VARMIN=333.0
+    VARMAX=354.0
+    VARINT=3.0
+    VARTYPE="seis"
+    BINT=a6f3
+elif test ${prefix} = "buoyancy" ; then
+    title="BUOYANCY"
+    unit="[m@+2@+/s@+2@+]"
+    VARMIN=-0.05
+    VARMAX=0.05
+    VARINT=0.005
+    VARTYPE="polar"
+    BINT=a0.05f0.005
+elif test ${prefix} = "winterp" ; then
+    title="VERTICAL VELOCITY"
+    unit="[m/s]"
+    VARMIN=-5.0
+    VARMAX=5.0
+    VARINT=1.0
+    VARTYPE="polar"
+    BINT=a2f1
+else
+    echo "automatically selected"
+    title="${prefix}"
+    auto=1
+fi
 
-
+# x and y label
 xlabel="Distance [km]"
 ylabel="Height [km]"
 
-XINT=a10f5
+# other parameters for drawing GMT
+XINT=a50f10
 YINT=a5f1
-RANGE=-50/50/0/15
-PRJ=x0.1/0.2
+RANGE=-100/100/0/15
+PRJ=x0.05/0.2
 
-wind=1
+# wind vector options
+wind=0
 VECTOROPT="-S50 -Q0.005/0.2/0.1n0.45 -G0 -I4/1"
 VECTORPOS=25
+
 
 #------------------------------------------
 # GMT setting
@@ -72,8 +111,11 @@ psfile=${infile%.nc4}.ps
 
 # psxy
 CPALET=cpalet.cpt
-#grd2cpt ${infile} > ${CPALET}
-unucpt ${infile%.nc4} ${VARMIN} ${VARMAX} ${VARINT} ${VARTYPE}
+if test ${auto} -eq 1 ; then
+    grd2cpt ${infile} > ${CPALET}
+else
+    unucpt ${infile%.nc4} ${VARMIN} ${VARMAX} ${VARINT} ${VARTYPE}
+fi
 grdimage ${infile} -J${PRJ} -R${RANGE} -C${CPALET} -X3.0 -Y3.0 ${gmtsta} > ${psfile}
 
 # psscale
@@ -81,8 +123,8 @@ gmtset ANOT_FONT_SIZE 8p
 gmtset TICK_LENGTH -0.1c
 psscale -D1.35/-0.6/2.75/0.2h -B${BINT}/:"${unit}": -C${CPALET} -N ${gmtcon} >> ${psfile}
 
+# grdvector
 if test ${wind} -eq 1 ; then
-    suffix=$(echo ${infile} | cut -d "_" -f 2)
     grdvector uinterp_${suffix} winterp_${suffix} -J -R ${VECTOROPT} ${gmtcon} >> ${psfile}
     psxy -R -J -Sv0.02/0.15/0.1 -L -G0 -W1 -N ${gmtcon} << EOF >> ${psfile}
   ${VECTORPOS} -3.5 0 0.4
@@ -94,24 +136,37 @@ fi
 
 # psbasemap
 gmtset TICK_LENGTH -0.15c
-psbasemap -J -R -B${XINT}:"${xlabel}":/${YINT}:"${ylabel}":SWne:."${title} FT = ${suffix%.nc4}": ${gmtcon} >> ${psfile}
+#psbasemap -J -R -B${XINT}:"${xlabel}":/${YINT}:"${ylabel}":SWne:."${title}": ${gmtcon} >> ${psfile}
+psbasemap -J -R -B${XINT}:"${xlabel}":/${YINT}:"${ylabel}":SWne ${gmtcon} >> ${psfile}
 
 # labels (pstext)
-# x     y   size angle font place comment
+#  x      y   size angle font place comment
 cat << EOF | pstext -R1/10/1/10 -Jx1.0 -N ${gmtend} >> ${psfile}
-#  0.5    8.15  12   0.0   0    ML    (b)
-  -0.25  -0.25   1   0.0   0    ML    .
-  -0.25   5.25   1   0.0   0    ML    .
-  12.0   -0.25   1   0.0   0    ML    .
-  12.0    5.25   1   0.0   0    ML    .
+   1.00   4.25  10   0.0    0    ML ${title}
+   8.65   4.25  10   0.0    0    ML FT = ${ftime}
+  11.025  4.25  10   0.0    0    MR [min]
+  -0.25  -0.25   1   0.0    0    ML .
+  -0.25   4.75   1   0.0    0    ML .
+  12.0   -0.25   1   0.0    0    ML .
+  12.0    4.75   1   0.0    0    ML .
 EOF
 
 # convert from ps to png
-ps2raster -Tg -A ${psfile}
+#ps2raster -Tg -A ${psfile}
 
 # convert from ps to pdf
-ps2raster -Tf -A ${psfile}
+#ps2raster -Tf -A ${psfile}
 
+#rm -f ${psfile}
 rm -f .gmt*
 
 done
+
+echo "ps2png..."
+#ls *.ps | parallel -j +0 ps2raster -Tg -A {}
+ls *.ps | parallel -j 6 ps2raster -Tg -A {}
+echo "ps2pdf..."
+#ls *.ps | parallel -j +0 ps2raster -Tf -A {}
+ls *.ps | parallel -j 6 ps2raster -Tf -A {}
+echo "done."
+rm -f *.ps
